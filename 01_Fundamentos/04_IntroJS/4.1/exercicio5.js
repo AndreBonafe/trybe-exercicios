d287049e6583de6853e3c060a3dae7781bdbd1f6
André <andre.bonaf@gmail.com>
2021-07-28 20:58:54 -0300

    feitos programas 1 a 11

---
+++ 01_Fundamentos/04_IntroJS/4.1/exercicio5.js
@@ -0,0 +1,135 @@
+//Primeiros 5 programas
+
+// let a = 7;
+// let b = 3;
+
+// console.log(a + b);
+// console.log(a - b);
+// console.log(a * b);
+// console.log(a / b);
+// console.log(a % b);
+
+
+//Programa 6
+
+// var a = 25;
+// var b = 36;
+
+// if (a > b) {
+//     console.log('o maior numero é ' +  a);
+// }
+// else {
+//     console.log('o maior numero é ' + b);
+// }
+
+
+//Programa 7
+
+// var a = 400;
+// var b = 400;
+// var c = 400;
+
+// if (a > b && a > c) {
+//     console.log('o maior numero é ' +  a);
+// }
+// else if (b > a && b > c){
+//     console.log('o maior numero é ' + b);
+// }
+// else if (c > a && c > b){
+//     console.log('o maior numero é ' + c);
+// }
+// else {
+//     console.log('todos os numeros tem o mesmo valor')
+// }
+
+
+//Programa 8
+
+// var classe = "knight";
+
+// switch (classe) {
+//     case 'mago':
+//         console.log('false');
+//         break;
+    
+//     case 'knight':
+//         console.log('true');
+//         break;
+
+//     default:
+//         console.log('zero')
+//         break;
+// }
+
+
+//Programa 9
+
+// let angulo1 = 30;
+// let angulo2 = 20;
+// let angulo3 = 130;
+
+// let eValido = false;
+
+
+// if (angulo1 + angulo2 + angulo3 == 180) {
+//         eValido = true;
+//     }
+//     else{
+//         eValido = false;
+//     }
+    
+// if (angulo1 < 0 || angulo2 < 0 || angulo3 < 0) {
+//         eValido = 'erro: numero inválido.';
+//     }
+//     else{
+//     }
+
+// console.log(eValido);
+
+
+//Programa 10
+
+// var piece = 'bishop';
+
+// switch (piece) {
+//     case 'bishop':
+//         console.log('diagonals');
+//         break;
+
+//     default:
+//         console.log('Erro: Peça Inválida');
+//         break;
+// }
+
+
+//Programa 11
+
+// var Porcentagem = 101;
+
+// if (Porcentagem >= 90 && Porcentagem <= 100) {
+//     Porcentagem = "A";
+
+// } else if (Porcentagem >= 80 && Porcentagem < 90) {
+//     Porcentagem = "B";
+
+// } else if (Porcentagem >= 70 && Porcentagem < 80) {
+//     Porcentagem = "C";
+
+// } else if (Porcentagem >= 60 && Porcentagem < 70) {
+//     Porcentagem = "D";
+
+// } else if (Porcentagem >= 50 && Porcentagem < 60) {
+//     Porcentagem = "E";
+
+// } else if (Porcentagem < 50) {
+//     Porcentagem = "F - Not Even Close";
+
+// } else if (Porcentagem < 0 || Porcentagem > 100){
+//     Porcentagem = "Erro: Valor Inválido!";
+
+// }
+// console.log(Porcentagem);
+
+
+//Programa 12
+
