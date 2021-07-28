bec627c0d9833f826a4b714a384bfd382008e38f
André <andre.bonaf@gmail.com>
2021-07-28 18:39:53 -0300

    feito exercicio 1

---
+++ 01_Fundamentos/04_IntroJS/4.1/exercicio1.js
@@ -0,0 +1,11 @@
+const myname = "André";
+
+const birthCity = "São Paulo";
+
+let birthYear = 1998;
+birthYear = 2030;
+birthCity = "Rio de Janeiro";
+
+console.log(myname);
+console.log(birthCity);
+console.log(birthYear);
