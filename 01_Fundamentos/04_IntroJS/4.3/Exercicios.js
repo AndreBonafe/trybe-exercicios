3938be87b4bc8ecc4ce32fe4f306f5becf653a1c
André <andre.bonaf@gmail.com>
2021-07-30 15:44:10 -0300

    feito para fixar 4.3

---
+++ 01_Fundamentos/04_IntroJS/4.3/Exercicios.js
@@ -0,0 +1,25 @@
+// // Utilizando o array abaixo, percorra-o somando todos os valores. 
+// // Caso o valor final seja maior que 15, imprima-o.
+// //  Caso seja igual ou menor que 15, imprima a mensagem: "valor menor que 16":
+
+// //Adicionar o array;
+// let fruits = [3, 4, 10, 1, 12];
+
+// //Criar uma variável com valor 0;
+// let somaValores = 0;
+
+// //Criar um loop que percorre o array;
+// for (let index = 0; index < fruits.length; index += 1){
+//     //Incrementar a variável com o valor correspondente a cada loop;
+//     somaValores = somaValores + fruits[index];
+// }
+// //Criar um if com a condição da variável ser maior que 15;
+// if (somaValores > 15){
+//     //Caso a variável obedeça a condição - Imprimir a variável;
+//     console.log(somaValores);
+// } //Caso não obedeça a condição;
+// else {
+//     //Imprimir a mensagem "valor menor que 16";
+//     console.log("valor menor que 16");
+// }
+
