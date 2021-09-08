79acf13eee0eb51407f12ea27694a4959abb15da
André <andre.bonaf@gmail.com>
2021-09-08 18:54:55 -0300

    exercicio 1 e 3

--- 01_Fundamentos/08_HOFS do JS6/8.1/script.js
+++ 01_Fundamentos/08_HOFS do JS6/8.1/script.js
@@ -20,4 +20,29 @@ const employeesData = (name) => {
   return employeeObj;
 }
 
-console.log(newEmployees(employeesData));
+// console.log(newEmployees(employeesData));
+
+// 2 - Desenvolva uma HOF que retorna o resultado de um sorteio. Esta HOF irá gerar um número aleatório entre 1 e 5 recebendo como parâmetros o número apostado e uma função que checa se o número apostado é igual ao número sorteado. O retorno da sua HOF deve ser uma string (Ex: "Tente novamente" ou "Parabéns você ganhou").
+
+const lottery = (number, chosenNumber) => {
+  if (number === chosenNumber) {
+    return 'Parabéns você ganhou!'
+  } else {
+    return 'Tente novamente...'
+  }
+};
+
+const randomNumber = (min, max) => {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+// console.log(lottery(randomNumber(1, 5), 4));
+
+// 3 - Crie uma HOF que receberá três parâmetros. O primeiro será um array de respostas corretas (Gabarito), o segundo será um array de respostas a serem verificadas (respostas da pessoa estudante) e o terceiro é uma função que checa se as respostas estão corretas e faz a contagem da pontuação final recebida pela pessoa estudante. Ao final a HOF deve retornar o total da contagem de respostas certas.
+// Quando a resposta for correta a contagem sobe 1 ponto, quando for incorreta desce 0.5 pontos, e quando não houver resposta ("N.A") não altera-se a contagem.
+
+const RIGHT_ANSWERS = ['A', 'C', 'B', 'D', 'A', 'A', 'D', 'A', 'D', 'C'];
+const STUDENT_ANSWERS = ['A', 'N.A', 'B', 'D', 'A', 'C', 'N.A', 'A', 'D', 'B'];
+
